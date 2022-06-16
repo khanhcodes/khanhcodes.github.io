6b4d7ee1334c1adea6e427265da2092321cecaa3
nhukhanhng <ngkhanh14.kn@gmail.com>
2022-06-16 18:07:30 -0400

    fix broken mobile menu

--- src/components/header/mobile/index.js
+++ src/components/header/mobile/index.js
@@ -7,17 +7,24 @@ function Mobile({ isOpen, setIsOpen }) {
       <div className="close-icon" onClick={() => setIsOpen(!isOpen)}>
         <i class="fi fi-br-cross"></i>
       </div>
-      <div className="mobile-options">
+      <div className="mobile-options" onClick={() => setIsOpen(!isOpen)}>
         <div className="mobile-option">
           <a href="#project">Projects</a>
         </div>
-        <div className="mobile-option">
-          <a href="#skill">Skills</a>
+        <div className="mobile-option" onClick={() => setIsOpen(!isOpen)}>
+          <a href="#about-detail">About me</a>
         </div>
-        <div className="mobile-option">
+        <div className="mobile-option" onClick={() => setIsOpen(!isOpen)}>
           <a href="#experience">Experience</a>
         </div>
-        <div className="mobile-option">
+        <div className="mobile-option" onClick={() => setIsOpen(!isOpen)}>
+          <a
+            href="https://drive.google.com/file/d/130Q85HsmCGcXMXQiw00mP5QQod_yGovi/view?usp=sharing"
+          >
+            Resume
+          </a>
+        </div>
+        <div className="mobile-option" onClick={() => setIsOpen(!isOpen)}>
           <a href="#contact">Contact</a>
         </div>
       </div>
