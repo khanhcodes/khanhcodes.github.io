07cfbc3be83b22c6d409624888a1e7c7793b4e71
nhukhanhng <ngkhanh14.kn@gmail.com>
2022-06-16 15:09:14 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9500d76f87e04b76c3b9e454d77fadb8.js"
+  "/precache-manifest.71424a1b1bc4637f3f5ce8567244cf5f.js"
 );
 
 workbox.clientsClaim();
